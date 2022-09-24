1b6e19852d285dfd280dabb78eed077246a12802
Andrey N <neonbones@icloud.com>
2022-09-24 18:29:47 +0300

    bugfix:refact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
-export default class Footer extends React.Component{
-  render(){
+export default function Footer (){
+
     return (
       <footer className="footer">
         <p className="footer__copyrighting">© 2022 Mesto Russia</p>
       </footer>
     );
-  }
+  
 }
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,38 +1,36 @@
 import React from 'react';
-export default class PopupWithForm extends React.Component{
-  constructor(props){
-    super(props)
-  }
+export default function PopupWithForm (props){
+  
 
   // closePopup = (evt) => {
   //   if(evt.target === evt.currentTarget || evt.target.classList.contains('popup__close')){this.props.onClose()}
   // }
 
-  render(){
+  
     return(
-      <div className={`popup popup_${this.props.name} ${this.props.isOpen}`} id="popup-edit" onClick={(evt) => {
-        if(evt.target === evt.currentTarget || evt.target.classList.contains('popup__close')){this.props.onClose()}
+      <div className={`popup popup_${props.name} ${props.isOpen}`} id="popup-edit" onClick={(evt) => {
+        if(evt.target === evt.currentTarget || evt.target.classList.contains('popup__close')){props.onClose()}
       }}>
           <div className="popup__container">
-            <h2 className="popup__title">{this.props.title}</h2>
+            <h2 className="popup__title">{props.title}</h2>
             <form
               className="popup__forms"
-              name={this.props.name}
+              name={props.name}
               id="form-edit"
               noValidate
             >
-              {this.props.children}
+              {props.children}
               <button
                 type="submit"
                 className="popup__button-save"
                 id="submit-profile"
               >
-                {this.props.button}
+                {props.button}
               </button>
             </form>
             <button className="popup__close" type="button" ></button>
           </div>
         </div>
     )
-  }
+  
 }
